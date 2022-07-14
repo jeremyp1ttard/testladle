6f9b8979d8e3588a77cf5cccd9804c61d878ec39
jeremyp1ttard <84363943+jeremyp1ttard@users.noreply.github.com>
2022-07-14 16:16:27 +0800

    react idiosyncacy

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,6 +1,6 @@
 
 const Button = ({text}) => {
-  return <button type='button' class="px-5 py-2">{text}</button>
+  return <button type='button' className="px-5 py-2">{text}</button>
 }
 
 export default Button;
