0f7076cca298d06f8f13fcc2794d77b4eaef2337
jeremyp1ttard <84363943+jeremyp1ttard@users.noreply.github.com>
2022-07-14 16:12:59 +0800

    ladle things

--- components/button/Button.stories.jsx
+++ components/button/Button.stories.jsx
@@ -4,9 +4,12 @@ import Button from "./Button";
 export const Primary = ({label, disabled}) => {
   return <Button text={label} />;
 };
+export const Secondary = ({label, disabled}) => {
+  return <Button text="SECONDARY BUTTON" />;
+};
 
 Primary.args = {
-    label: "Hello world",
+    label: "Primary Button",
     disabled: false,
   };
   
